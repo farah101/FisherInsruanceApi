649911c486616a40a58e1be50faea711c47fa5a8
yusuf <yusuf>
2017-02-27 20:14:26 -0500

    lab 5 update

---
+++ wwwroot/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({    
+    selector: "fisher-insurance",     
+    templateUrl:`./app/app.component.html`
+
+})
+
+export class AppComponent { }
